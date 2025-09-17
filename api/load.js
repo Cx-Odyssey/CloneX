06f69b8f50b8890790518613f4bdcfb991a657ff
Rahman <muntasixyz@gmail.com>
2025-09-17 16:38:02 +0600

    Delete api/load.js

--- api/load.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const { playerId } = req.query;
-  globalThis.db = globalThis.db || {};
-  const state = globalThis.db[playerId] || null;
-
-  res.status(200).json({ state });
-}
