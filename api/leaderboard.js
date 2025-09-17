99fd76d5f2aa768aa8c02391a5770e46749ffb5b
Rahman <muntasixyz@gmail.com>
2025-09-17 16:37:48 +0600

    Delete api/leaderboard.js

--- api/leaderboard.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  globalThis.db = globalThis.db || {};
-  const leaderboard = Object.entries(globalThis.db)
-    .map(([id, state]) => ({ id, gp: state.gp || 0 }))
-    .sort((a, b) => b.gp - a.gp)
-    .slice(0, 10);
-
-  res.status(200).json({ leaderboard });
-}
