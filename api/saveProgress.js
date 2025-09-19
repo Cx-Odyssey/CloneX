aea837c26915bc86e84dc7872148e13bc0db9b03
Rahman <muntasixyz@gmail.com>
2025-09-19 10:47:58 +0600

    Update saveProgress.js

--- api/saveProgress.js
+++ api/saveProgress.js
@@ -1,4 +1,4 @@
-import supabase from './supabaseClient';
+import supabase from './supabaseClient.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -27,6 +27,7 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'Unexpected server error' });
   }
 }
+
 
 
 
